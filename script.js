68efdcefb7f3caf368e2df55e148a9a15d91d174
fazchanneltv <38880224+fazchanneltv@users.noreply.github.com>
2021-07-19 03:41:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var audio;
 var playlist;
 var tracks;
 var current;
+var mylink;
+
 
 function init() {
   current = 0;
@@ -15,20 +17,20 @@ function init() {
   len = tracks.length - 1;
   playlist.find('a').click(function(e) {
     e.preventDefault();
-    var link = $(this);
-    current = link.parent().index();
-    run(link, audio[0]);
+    mylink = $(this);
+    current = mylink.parent().index();
+    run(mylink, audio[0]);
   });
 
   audio[0].addEventListener('ended', function(e) {
     current++;
     if (current > len) {
       current = 0;
-      link = playlist.find('a')[0];
+      mylink = playlist.find('a')[0];
     } else {
-      link = playlist.find('a')[current];
+      mylink = playlist.find('a')[current];
     }
-    run($(link), audio[0]);
+    run($(mylink), audio[0]);
   });
 };
 init();
