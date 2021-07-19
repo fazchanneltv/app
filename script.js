1238626cdc6bc53384f8840936f300ee654c47fb
fazchanneltv <38880224+fazchanneltv@users.noreply.github.com>
2021-07-19 02:18:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ function run(link, player) {
   audio[0].play();
 };
 
-    /* >>> Histats <<<
+/* >>> Histats <<<
       =======================*/
-  var _Hasync = _Hasync || [];
-  _Hasync.push(['Histats.start', '1,4442247,4,107,170,20,00011111']);
-  _Hasync.push(['Histats.fasi', '1']);
-  _Hasync.push(['Histats.track_hits', '']);
+var _Hasync = _Hasync || [];
+_Hasync.push(['Histats.start', '1,4442247,4,107,170,20,00011111']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
