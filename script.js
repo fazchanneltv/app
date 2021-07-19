97d31bea57e2610535e810dd39334579775e4426
fazchanneltv <38880224+fazchanneltv@users.noreply.github.com>
2021-07-19 01:28:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -47,16 +47,3 @@ function run(link, player) {
   _Hasync.push(['Histats.start', '1,4442247,4,107,170,20,00011111']);
   _Hasync.push(['Histats.fasi', '1']);
   _Hasync.push(['Histats.track_hits', '']);
-
-  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-    /* >>> Marquee JQuery <<<
-      =======================*/
-  $('.marquee').marquee({
-    duration: 8000,
-    gap: 2,
-    delayBeforeStart: 0,
-    direction: 'left',
-    pauseOnHover: 'true',
-    duplicated: 'false',
-  });
